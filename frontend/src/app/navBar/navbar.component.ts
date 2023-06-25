386bcd9dd457dfbfdc1ac76b6827ffd9157872be
Fayjul <fayzulislam6@gmail.com>
2023-06-25 07:27:45 +0600

    navbar components from screach is done

--- frontend/src/app/navBar/navbar.component.ts
+++ frontend/src/app/navBar/navbar.component.ts
@@ -2,12 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navbar',
-    template: `<h1>This is nav </h1>
-            <p>Hello Bangladesh</p>`,
-    styles: [`h1{
-         color:red;
-         font-size: 100px;
-    }`]
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 
 export class NavbarComponent{
