aaa4c3792408391c18d924e36b43a47c0aa0dcf0
Fayjul <fayzulislam6@gmail.com>
2023-06-25 06:59:38 +0600

    50 minutes done

--- frontend/src/app/navBar/navbar.component.ts
+++ frontend/src/app/navBar/navbar.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navbar',
-    template: `<h1>This is nav </h1>`
+    template: `<h1>This is nav </h1>
+            <p>Hello Bangladesh</p>`,
+    styles: [`h1{
+         color:red;
+         font-size: 100px;
+    }`]
 })
 
 export class NavbarComponent{
