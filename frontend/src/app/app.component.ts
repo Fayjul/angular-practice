fa5e124f1354fc052857f553f99c3d4c87dee186
Fayjul <fayzulislam6@gmail.com>
2023-07-04 06:39:18 +0600

    Data sharing between parent to child is work now

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  parentMessage: string = "Message coming from parent components";
 }
--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
   title: string = "List of Posts";
+  messagePost: string = "Message Post";
+
+  @Input() fromParent: string = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
