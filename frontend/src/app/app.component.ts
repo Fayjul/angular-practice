45ad54ed7cb48bb6a8f1768426b4d48deaa372f9
fayjul <fayjul79@gmail.com>
2023-07-06 06:25:43 +0600

    app.component.ts file is updated for display data.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,31 +1,12 @@
-import { Component, ViewChild , AfterViewInit} from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'frontend';
-  parentMessage: string = "Message Changed";
-  message : string = '';
-  fromChildOutput : string = '';
-
-  @ViewChild(PostComponent) childComp: any;
-
-  constructor(){
-    console.log(this.childComp);
-    
-  }
-  ngAfterViewInit(): void {
-    console.log(this.childComp);
-    this.message = this.childComp.childMessage;
-    console.log("hello " + this.message);
-    
-  }
-
-  reciveMessage($event : any){
-    console.log("from reciveMessage " + $event);
-    this.fromChildOutput = $event;
-  }
+  message = 'This is from app.component.ts'
+  
 }
